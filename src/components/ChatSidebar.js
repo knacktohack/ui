c6fd9e7f4e8b6ce9474b707b08e3cc9339bbe651
Adnan Khurshid <adnankhurshid251@gmail.com>
2024-04-15 06:34:49 +0530

    chat management 2nd iteration

--- src/components/ChatSidebar.js
+++ src/components/ChatSidebar.js
@@ -4,44 +4,38 @@ import { Link } from "react-router-dom";
 import { backendUrl } from "../constants";
 import NewChatButton from "./NewChatButton";
 
-const ChatSidebar = (props) => {
-  const conversation_id = props.conversationId;
-    const user_id = "12345"
-  const [sessions ,setSessions] = useState([])
+const ChatSidebar = ({ conversationId, shouldRerender }) => {
+  const user_id = "12345";
+  const [sessions, setSessions] = useState([]);
 
-  const fetchSessions = async () =>{
-      try {
-        const response = await axios.get(
-          `${backendUrl}/history/${user_id}`,
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        setSessions(response.data.response);
-      } catch (error) {
-        console.log("Could not fetch session heads");
-      }
-
-  }
+  const fetchSessions = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/history/${user_id}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setSessions(response.data.response.reverse());
+    } catch (error) {
+      console.log("Could not fetch session heads");
+    }
+  };
 
   useEffect(() => {
     fetchSessions();
-  }, []);
-
+  }, [shouldRerender]); // Re-fetch sessions when shouldRerender changes
 
   return (
-    <div className="h-full w-1/5 border border-green-primary bg-green-primary flex flex-col gap-10 p-4 text-white tracking-wide">
-      <div className="flex flex-col w-full gap-4">
-        <div className="w-full flex flex-row justify-center items-center bg-orange-primary rounded-md p-2">
+    <div className="h-full w-1/5 border border-green-primary bg-green-primary flex flex-col gap-10 p-2 text-white tracking-wide">
+      <div className="flex flex-col w-full gap-4 px-2">
+        <div className="w-full flex flex-row justify-center items-center border-2 border-orange-primary rounded-md p-2">
           adnan.khurshid@company.com
         </div>
 
-        <NewChatButton/>
+        <NewChatButton />
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 overflow-y-scroll px-2">
         <div className="text-lg font-medium tracking-wider">
           Previous Sessions
         </div>
@@ -50,9 +44,7 @@ const ChatSidebar = (props) => {
             key={index}
             to={`/chat/c/${item.conversation_id}`}
             className={`w-full p-2 border-2 pointer border-orange-primary hover:bg-orange-primary rounded-md transition duration-150 ease-in-out truncate ${
-              conversation_id === item.conversation_id
-                ? "bg-orange-primary"
-                : ""
+              conversationId === item.conversation_id ? "bg-orange-primary" : ""
             } `}
           >
             {item.title}
--- src/pages/ChatSession.js
+++ src/pages/ChatSession.js
@@ -6,16 +6,14 @@ import ChatSidebar from "../components/ChatSidebar";
 import { backendUrl } from "../constants";
 
 const ChatSession = () => {
-
-    const location = useLocation();
-    const currentUrl = location.pathname;
-    const conversation_id = currentUrl.split('/')[3];
-
-    const user_id = "12345"
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  const conversation_id = currentUrl.split('/')[3];
+  const user_id = "12345";
 
   const [messages, setMessages] = useState([]);
-
   const [prompt, setPrompt] = useState("");
+  const [shouldRerenderSidebar, setShouldRerenderSidebar] = useState(false); // State to track whether to rerender sidebar
 
   const messagesEndRef = useRef(null);
 
@@ -23,48 +21,44 @@ const ChatSession = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-
-
-
   useEffect(() => {
-    const fetchMessages = async () =>{
-        try {
-          const response = await axios.get(
-            `${backendUrl}/history/${user_id}/${conversation_id}`,
-            {
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-          setMessages(response.data.response);
-        } catch (error) {
-          console.log("Could not generate response");
-        }
-    }
+    const fetchMessages = async () => {
+      try {
+        const response = await axios.get(
+          `${backendUrl}/history/${user_id}/${conversation_id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setMessages(response.data.response);
+      } catch (error) {
+        console.log("Could not generate response");
+      }
+    };
     fetchMessages();
   }, [conversation_id]);
 
-
   useEffect(() => {
     scrollToBottom();
+      // Set the state to rerender sidebar
+      setShouldRerenderSidebar(!shouldRerenderSidebar);
   }, [messages]);
 
   const sendPrompt = async () => {
     const obj = {
-      'content' : prompt,
-      'type' : 'human'
-    }
+      content: prompt,
+      type: "human",
+    };
     setMessages((prevMessages) => [...prevMessages, obj]);
     setPrompt("");
 
     const reqBody = {
-      'prompt': prompt,
-      'user_id': '12345',
-      'conversation_id': conversation_id,
+      prompt: prompt,
+      user_id: "12345",
+      conversation_id: conversation_id,
     };
-
-    console.log(reqBody)
 
     try {
       const response = await axios.post(
@@ -76,8 +70,8 @@ const ChatSession = () => {
           },
         }
       );
-      
-      setMessages((prevMessages) => [...prevMessages, {content:response.data.response,type:'ai'}]);
+
+      setMessages((prevMessages) => [...prevMessages, { content: response.data.response, type: "ai",flagged : response.data.status === 400 }]);
       scrollToBottom();
     } catch (error) {
       toast.error("Could not generate response");
@@ -86,13 +80,14 @@ const ChatSession = () => {
 
   return (
     <div className="h-screen w-screen flex flex-row">
-      <ChatSidebar conversationId ={conversation_id} />
+      {/* Pass the shouldRerenderSidebar state as a prop */}
+      <ChatSidebar conversationId={conversation_id} shouldRerender={shouldRerenderSidebar} />
       <div className="h-full w-4/5 flex flex-col justify-start gap-2 items-center relative">
         <div className="w-full bg-green-primary text-white text-2xl font-semibold tracking-wide flex flex-row justify-center items-center p-3 ">
           Mojo-GPT
         </div>
         <div className="h-4/6 w-9/12 flex flex-col gap-4 justify-start text-white overflow-y-scroll px-4 py-8">
-          {messages.length===0 && (
+          {messages.length === 0 && (
             <div className="text-neutral-600 text-2xl flex flex-col justify-center items-center">
               Start by asking something...
             </div>
@@ -101,7 +96,10 @@ const ChatSession = () => {
             <div
               key={index}
               className={
-                item.type === 'human'
+                item.flagged ?
+                "border-2 border-red-500 text-red-500 bg-red-200 max-w-fit rounded-md p-2 mr-32 self-start"
+                :
+                item.type === "human"
                   ? "bg-green-primary max-w-fit rounded-md p-2 ml-32 self-end"
                   : "border-2 border-green-primary text-green-primary max-w-fit rounded-md p-2 mr-32 self-start"
               }
@@ -121,7 +119,8 @@ const ChatSession = () => {
           />
           <button
             onClick={sendPrompt}
-            className="w-1/12 bg-orange-primary h-10 text-white rounded-md"
+            className="w-1/12 bg-orange-primary h-10 text-white rounded-md cursor-pointer disabled:bg-orange-200"
+            disabled={prompt.trim() === ""}
           >
             SEND
           </button>
